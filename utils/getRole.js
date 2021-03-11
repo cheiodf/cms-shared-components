01cd7085d703641690e53acd235f0cc97c2f23a6
germancutraro <cutraroger@gmail.com>
2021-03-10 23:51:50 -0300

    getrouteroles

--- utils/getRole.js
+++ utils/getRole.js
@@ -1,5 +1,6 @@
 import { parseCookies } from 'nookies';
 import jwtDecode from 'jwt-decode';
+import { navigationFlat } from './navigationFlat';
 
 const getRole = () => {
   const cookies = parseCookies();
@@ -7,9 +8,17 @@ const getRole = () => {
     const { role } = jwtDecode(cookies.token, {
       payload: true
     });
-
     return role;
   }
 };
-
+export const getRouteRoles = (path, params) => {
+  const nav = navigationFlat.find(route => {
+    const a = params && Object.keys(params)[0]; // slug
+    let newPath = route.path;
+    if (a) newPath = route.path.replace(`[${a}]`, params[a]) + '/';
+    else newPath = route.path + '/';
+    return newPath === path;
+  });
+  return nav;
+};
 export default getRole;
