1e6f7930029fef4a4b295fd7f81e37d900e403d8
germancutraro <cutraroger@gmail.com>
2021-03-22 18:28:02 -0300

    useAuth

---
+++ hooks/useAuth.js
@@ -0,0 +1,32 @@
+import nookies from 'nookies';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getRouteRoles } from '../utils/getRole';
+
+// RINA = Redirect If Not Authenticated
+// RIIA = Redirect If Is Authenticated
+
+function useAuth(action = 'RINA', roles = false) {
+  const { token, userInfo } = nookies.get();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (roles) {
+      const user = JSON.parse(userInfo);
+      let path = router.pathname;
+      if (!path.endsWith('/')) path += '/';
+
+      if (!getRouteRoles(path)?.roles.includes(user.role))
+        router.push('/dashboard');
+    }
+
+    if (!token && action === 'RINA') router.push('/');
+    else if (token && action === 'RIIA') router.push('/dashboard');
+  }, [token]);
+
+  return {
+    isAuth: !!token
+  };
+}
+
+export default useAuth;
