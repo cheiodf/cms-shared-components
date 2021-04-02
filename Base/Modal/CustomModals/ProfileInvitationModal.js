01bc515d31576a418ee98a14e7c7a3d4f9ed2a49
Mateo Klocke <mateo.klocke@gmail.com>
2021-04-02 19:45:09 -0300

    profile petitions modal

--- Base/Modal/CustomModals/ProfileInvitationModal.js
+++ Base/Modal/CustomModals/ProfileInvitationModal.js
@@ -1,6 +1,10 @@
+// import { useForm } from 'react-hook-form';
+
+// import Form from '../../Form/Form';
 import Textarea from '../../Input/Textarea/Textarea';
 import Typography from '../../Typography/Typography';
 import ModalButtons from '../ModalButtons';
+// import Button from '../../Button/Button';
 
 const ProfileInvitationModal = ({
   title,
@@ -9,22 +13,33 @@ const ProfileInvitationModal = ({
   handleClose,
   buttonAcceptTitle,
   loading,
-  size
+  size,
+  // onSubmit,
+  // setMessage
 }) => {
+  // const { register, handleSubmit } = useForm();
+
   return (
     <div>
       <Typography tag="h2">{title}</Typography>
       <Typography>{description}</Typography>
 
-      <Textarea name="message" />
-
-      <ModalButtons
-        onClose={handleClose}
-        onAccept={onAccept}
-        buttonAcceptTitle={buttonAcceptTitle}
-        disabled={loading}
-        size={size}
-      />
+     {/* <Form onSubmit={handleSubmit(onSubmit)}> */}
+        <Textarea 
+          name="message"
+          placeholder="Escribe tu mensaje"
+          // onChange={(e) => setMessage(e.target.value)}
+          // resgister={register()}
+        />
+  
+        <ModalButtons
+          onClose={handleClose}
+          onAccept={onAccept}
+          buttonAcceptTitle={buttonAcceptTitle}
+          disabled={loading}
+          size={size}
+        />
+     {/* </Form> */}
     </div>
   );
 };
---
+++ Base/Modal/CustomModals/ProfilePetitionsModal.js
@@ -0,0 +1,80 @@
+import Typography from '../../Typography/Typography';
+import ModalButtons from '../ModalButtons';
+import Avatar from '../../Avatar/Avatar';
+import Button from '../../Button/Button';
+import Divider from '../../Divider/Divider';
+import Grid from '../../Grid/Grid';
+import GridItem from '../../Grid/GridItem';
+import {
+  PetitionContainer,
+  PetitionButtons,
+} from '../modalStyles';
+
+const ProfilePetitionsModal = ({
+  title,
+  list = [],
+  handleClose,
+  buttonCloseTitle,
+  loading,
+  size,
+  type,
+  onAction
+}) => {
+  return (
+    <div>
+      <Typography tag="h3">{title}</Typography>
+
+      {console.log('list', list)}
+  
+      <div>
+        {list.length ? list?.map(petition => 
+          <PetitionContainer key={petition[type]._id}>
+            <Grid
+              cols={8}
+              rows={1}
+              columnGap={2}
+            >
+              <GridItem cols={2} rows={1}>
+                <Avatar src={petition[type].avatar} isEditable={false} size="sm" />
+              </GridItem>
+              <GridItem cols={6} rows={1}>
+                <Typography margin="0 0 2rem 1rem" weight="bold">{petition[type].name}</Typography>
+                <Typography margin="0 0 0 1rem" style={{ opacity: 0.6 }} maxLines={2} onHover={{maxLines: 0}} wordWrap={true}>{petition.message}</Typography>
+                <PetitionButtons>
+                  <Button
+                    size="sm"
+                    type="button"
+                    margin="2rem 0 1rem 1rem"
+                    variant="primary"
+                    onClick={() => onAction(petition[type]._id, 'accepted')}
+                  >
+                    Aceptar
+                  </Button>
+                  <Button
+                    size="sm"
+                    type="button"
+                    margin="2rem 0 1rem 1rem"
+                    variant="dark"
+                    onClick={() => onAction(petition[type]._id, 'rejected')}
+                  >
+                  Rechazar
+                  </Button>
+                </PetitionButtons>
+              </GridItem>
+            </Grid>
+            <Divider />
+          </PetitionContainer>
+        ) : ''}
+      </div>
+  
+      <ModalButtons
+        onClose={handleClose}      
+        buttonCloseTitle={buttonCloseTitle}
+        disabled={loading}
+        size={size}
+      />
+    </div>
+  );
+};
+
+export default ProfilePetitionsModal;
